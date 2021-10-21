f9775e104381cb08268a877c524778ad95ffacba
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2021-10-21 18:25:54 -0400

    Multi-Layer Perceptron Test file

---
+++ MLP/MLPtest.py
@@ -0,0 +1,36 @@
+"""
+Test harness for MLP.py
+"""
+
+from MLP import MLP
+import os
+import sys
+import numpy as np
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+from mlutils import MathML, split_inputs_labels
+from datagen import DataGen
+
+
+class MLPTest:
+
+    def __init__(self):
+        dg = DataGen()              # Generates dataset(s)
+        self.math = MathML()        # Math for machine learning
+        n_samples = 35
+
+        # Initialize datasets
+        self.raw_dataset = dg.bmi_dataset(n_samples)    
+        
+        mlp = MLP(self.raw_dataset, n_hidden=3)
+
+        mlp.print_self()
+        prediction = mlp.feed_forward()
+        label = mlp.train_data['labels'][0]
+        loss = mlp.loss_func(prediction, label)
+        print("\n\nModel's initial prediction on first sample:")
+        print("Prediction: " + str(prediction) + "\tLabel: " + str(label))
+        print("Loss: " + str(loss))
+
+
+
+tester = MLPTest()
